993517554697b51158c8330b08387057ac604efb
Paralevi Jheng <Paralevi@live.com>
2017-04-02 15:15:37 +0800

    fully remove networkx function for better performance and reconstructed in OOP

---
+++ codes/linkpred.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import numpy as np
+import heapq
+from scipy import sparse
+from sklearn.model_selection import train_test_split
+
+class link_prediction(object):
+    def __init__(self, E_train, E_test):
+        node_size = 0
+
+        for e in E_train:
+            node_size = max(node_size,e[0],e[1])
+        for e in E_test:
+            node_size = max(node_size,e[0],e[1])
+
+        self.M_train, self.adj_train = self._build_matrix(E_train,node_size)
+        _,self.adj_test = self._build_matrix(E_test,node_size)
+
+        self.pred = None
+        self.predCN = None
+        self.predJC = None
+
+    def _build_matrix(self, E, m_size):
+        l_size = len(E)
+
+        val = np.ones(l_size<<1,np.int8)
+        col = np.zeros(l_size<<1,np.int16)
+        row = np.zeros(l_size<<1,np.int16)
+        # create adjacent sparse matrix
+        for i,e in enumerate(E):
+            u,v = e[0],e[1]
+            row[i<<1] = col[i<<1|1] = u
+            col[i<<1] = row[i<<1|1] = v
+
+        M = sparse.coo_matrix((val, (row, col)), shape=(m_size+1, m_size+1))
+        del val,col,row
+        return M,M.toarray()
+
+    def common_neighbors(self,K,range=200):
+        if self.predCN is not None:
+            self.pred = self.predCN[:K]
+            return self.pred
+        '''
+        calculate the number of common neighbors by matrix multiplication
+        much faster than networkx built-in func
+        '''
+        M2 = self.M_train.dot(self.M_train).tolil()
+        M2.setdiag(0)
+        row,col = M2.nonzero()
+        index = zip(row,col)
+        pred = list(filter(lambda e: not self.adj_train[e[1],e[2]] and e[1]<e[2],[(M2[u,v],u,v) for u,v in index]))
+        self.predCN = list(map(lambda x:(x[1],x[2],x[0]), heapq.nlargest(range,pred)))
+        del M2,pred,index,row,col
+        self.pred = self.predCN[:K]
+        return self.pred
+
+    def jaccard_coefficient(self,K,range=200):
+        if self.predJC is not None:
+            self.pred = self.predJC[:K]
+            return self.pred
+        M2 = self.M_train.dot(self.M_train).tolil()
+        M2.setdiag(0)
+        t=self.M_train.sum(axis=0)
+        row,col=M2.nonzero()
+        index=zip(row,col)
+        del row,col
+        max_heap=[]
+        for i,j in index:
+            if M2[i,j]==0 or self.adj_train[i,j]:
+                continue
+            M2[j,i]=0
+            jc = M2[i,j]/(t[0,i]+t[0,j]-M2[i,j])
+            max_heap.append((jc,i,j))
+        # pred = np.array(np.unravel_index(np.argsort((M.A).flatten(),axis=0)[-K:],M.shape)).T[::-1]
+        self.predJC = list(map(lambda x:(x[1],x[2],x[0]), heapq.nlargest(range,max_heap)))
+        self.pred = self.predJC[:K]
+        return self.pred
+
+    def test_precision(self,detail=False):
+        L = self.pred
+        correct = 0
+        for u,v,c in L:
+            check = self.adj_test[u,v] != 0
+            if detail == True:
+                print(u,v,c,check)
+            if check == True:
+                correct = correct+1
+        precision = correct/len(L)
+        return precision
--- codes/pJaccardCoefficient.py
+++ codes/pJaccardCoefficient.py
@@ -1,76 +1,18 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_curve, auc
 import pandas as pd
-import networkx as nx
-import numpy as np
-from scipy import sparse
-from sklearn.cross_validation import train_test_split
-import heapq
-K = 20
-
-def jaccard_coefficient(G):
-    l_size,m_size = G.number_of_edges(), max(G.nodes())
-    E=G.edges()
-    val = np.ones(l_size<<1,np.float16)
-    col = np.zeros(l_size<<1,np.int16)  
-    row = np.zeros(l_size<<1,np.int16)  
-    for i in range(l_size):
-        row[i<<1] = col[i<<1|1] = E[i][0]
-        col[i<<1] = row[i<<1|1] = E[i][1]
-    M = sparse.coo_matrix((val, (row, col)), shape=(m_size+1, m_size+1))
-    M2 = M.dot(M).tolil()
-    M2.setdiag(0)
-    t=M.sum(axis=0)
-    row,col=M2.nonzero()
-    index=zip(row,col)
-    del row,col
-    max_heap=[]
-    M = sparse.lil_matrix(M.shape)
-    print('calculating...')
-    for i,j in index:
-        if M2[i,j] == 0 or G.has_edge(i,j):
-            continue
-        M2[j,i] = 0
-        M[i,j]=M2[i,j]/(t[0,i]+t[0,j]-M2[i,j])
-        if len(max_heap) <= K:
-            heapq.heappush(max_heap,(-M[i,j],i,j))
-        else:
-            heapq.heappushpop(max_heap,(-M[i,j],i,j))
-    # pred = np.array(np.unravel_index(np.argsort((M.A).flatten(),axis=0)[-K:],M.shape)).T[::-1]
-    return list(map(lambda x:(x[1],x[2],-x[0]), max_heap))
-
-def networkx(G):
-    max_heap=[]
-    L=nx.jaccard_coefficient(G)
-    for u,v,j in L:
-        if G.has_edge(u,v):
-            continue
-        if len(max_heap) <= K:
-            heapq.heappush(max_heap,(-j,u,v))
-        else:
-            heapq.heappushpop(max_heap,(-j,u,v))
-    return list(map(lambda x:(x[1],x[2],-x[0]), max_heap))
-
+import linkpred as lp
+import cProfile
 def main():
-    data = pd.read_csv('../YouTube-dataset/1-edges.csv',names=['u','v','w'])
+    data = pd.read_csv('../YouTube-dataset/data/1-edges.csv',names=['u','v','w'])
     X_train,X_test,y_train,y_test=train_test_split(data[['u','v']],data['w'],test_size=0.1)
+    T = lp.link_prediction(X_train.values.tolist(),X_test.values.tolist())
 
-    G_train, G_test = nx.Graph(),nx.Graph()
-    G_train.add_edges_from(X_train.values.tolist())
-    G_test.add_edges_from(X_test.values.tolist())
-
-    L = jaccard_coefficient(G_train)
-    '''
-    very slow! more than half an hour in test
-    L = networkx(G_train)
-    '''
-    correct,i = 0,0
-    for u,v,j in L:
-        print(u,v,j,end=' ')
-        if G_test.has_edge(u,v):
-            correct = correct+1
-            print('T')
-        else:
-            print('F')
-    print('Accuracy:',correct/len(L))
+    K=[10,20,30,50,100]
+    for k in K:
+        T.jaccard_coefficient(k)
+        print('K=%d,Precision=%.2f' % (k,T.test_precision()))
 
 if __name__=='__main__':
+    #cProfile.run('main()')
     main()
